8be433927c19d81f08e9695a05b915f0516974c3
baekCode <baekcode@gmail.com>
2020-12-09 00:00:59 +0900

    feat : detail comments

--- crud/client/src/components/Detail/index.jsx
+++ crud/client/src/components/Detail/index.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Comments from '../Comments';
 
-Detail.propTypes = {};
+Detail.propTypes = {
+  detailData  : PropTypes.object,
+  commentsData: PropTypes.oneOfType([PropTypes.object, PropTypes.array])
+};
 
 const Container = styled.div`
   
 `;
 
-function Detail({detailData}) {
+function Detail({detailData, commentsData}) {
   const {id, title, author} = detailData;
+
   return (
     <Container>
       <h2>상세페이지</h2>
       <p>No : {id}</p>
       <p>Title : {title}</p>
       <p>Author :{author}</p>
+      {!!commentsData.length && <Comments commentsData={commentsData}/>}
     </Container>
   );
 }
