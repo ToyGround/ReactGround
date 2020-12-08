f125258e974a54bce4d8f160b51a018d74e230e1
baekCode <baekcode@gmail.com>
2020-12-09 00:01:15 +0900

    feat : comments

--- crud/client/src/components/Comments/index.jsx
+++ crud/client/src/components/Comments/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 Comments.propTypes = {
-
+  commentsData : PropTypes.array
 };
 
-function Comments(props) {
+const Container = styled.div``
+
+function Comments({commentsData}) {
   return (
-    <div>댓글 목록</div>
+    <Container>
+      <h3>댓글</h3>
+      {commentsData.map(item => (
+        <p key={item.id}><span>{item.id}번 댓글 : </span>{item.body}</p>
+      ))}
+    </Container>
   );
 }
 
