c3fbab8f03d07428d061ae2e8bc4f61f7e7e259a
baekCode <baekcode@gmail.com>
2020-12-08 22:50:33 +0900

    feat : detail component

--- crud/client/src/components/Detail/index.jsx
+++ crud/client/src/components/Detail/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-Detail.propTypes = {
+Detail.propTypes = {};
 
-};
+const Container = styled.div`
+  
+`;
 
-function Detail(props) {
+function Detail({detailData}) {
+  const {id, title, author} = detailData;
   return (
-    <div>상세페이지</div>
+    <Container>
+      <h2>상세페이지</h2>
+      <p>No : {id}</p>
+      <p>Title : {title}</p>
+      <p>Author :{author}</p>
+    </Container>
   );
 }
 
