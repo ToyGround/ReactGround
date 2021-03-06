a348f3b826c6c248366d8041e6c52284dc0a1790
baekCode <baekcode@gmail.com>
2021-03-06 22:49:18 +0900

    feat : api call test

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,7 +1,26 @@
+import {useState} from 'react';
+import axios from 'axios';
+import config from './config';
+
+const {URL, API_KEY} = config;
+
 function App() {
+  const [data, setData] = useState(null);
+
+  const onClick = async () => {
+    try {
+      const response = await axios(`${URL + API_KEY}`);
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <>
       <h1>News Viewer</h1>
+      <button onClick={onClick}>불러오기</button>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly/>}
     </>
   );
 }
