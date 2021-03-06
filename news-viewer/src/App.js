7109aa563d8de7b3627b099441bd2b9aa571a22c
baekCode <baekcode@gmail.com>
2021-03-07 00:08:48 +0900

    feat : props category & callback

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,10 +1,15 @@
+import {useCallback, useState} from 'react';
 import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
 function App() {
+  const [category, setCategory] = useState('all');
+  const onClick = useCallback(category => setCategory(category), []);
   return (
     <>
       <h1>News Viewer</h1>
-      <NewsList />
+      <Categories category={category} onClick={onClick}/>
+      <NewsList category={category}/>
     </>
   );
 }
