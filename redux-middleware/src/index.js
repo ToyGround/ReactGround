6d7d9b08e9c5a0632c0ea3187bfc1aff0bdcdd35
baekCode <baekcode@gmail.com>
2021-03-07 13:37:58 +0900

    feat : index counter saga 적용

--- redux-middleware/src/index.js
+++ redux-middleware/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {applyMiddleware, createStore} from 'redux';
 import {Provider} from 'react-redux';
-import ReduxThunk from 'redux-thunk'
 import {createLogger} from 'redux-logger';
 import App from './App';
-import rootReducer from './modules';
+import rootReducer, {rootSaga} from './modules/sagas';
+import createSagaMiddleware from 'redux-saga';
 
-const logger = createLogger()
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
+const logger = createLogger();
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, applyMiddleware(logger, sagaMiddleware));
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
