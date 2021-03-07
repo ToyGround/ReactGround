1b63fda2f29620a6bc0607d0dc96d87e06902e8f
baekCode <baekcode@gmail.com>
2021-03-07 13:03:19 +0900

    modify : 오타

--- redux-middleware/src/components/Counter.js
+++ redux-middleware/src/components/Counter.js
@@ -5,7 +5,7 @@ function Counter({onIncrease, onDecrease, number}) {
     <>
       <h1>{number}</h1>
       <button onClick={onIncrease}>+ 1</button>
-      <button onClick={onDecrease}>+ 1</button>
+      <button onClick={onDecrease}>- 1</button>
     </>
   );
 }
