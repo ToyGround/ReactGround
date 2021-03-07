5bcf4936982350cb75e57e7da9cc1031727f8fb3
baekCode <baekcode@gmail.com>
2021-03-07 12:59:10 +0900

    feat : 비동기통신을 위한 reduxThunk 함수 적용

--- redux-middleware/src/modules/counter.js
+++ redux-middleware/src/modules/counter.js
@@ -6,6 +6,21 @@ const DECREASE = 'counter/DECREASE';
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
+/**
+ * ReudxThunk function
+ * @description reduxThunk function, 비동기 통신을 위해 setTimeOut 처리함
+ * */
+export const increaseThunk = () => dispatch => {
+  setTimeout(() => {
+    dispatch(increase());
+  }, 1000);
+};
+export const decreaseThunk = () => dispatch => {
+  setTimeout(() => {
+    dispatch(decrease());
+  }, 1000);
+};
+
 const initialState = 0;
 
 const counter = handleActions({
