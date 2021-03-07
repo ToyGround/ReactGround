90cb941ddabd41850444e8ed24d1a9134d6fe561
baekCode <baekcode@gmail.com>
2021-03-07 12:45:26 +0900

    feat : add redux logger

--- redux-middleware/src/index.js
+++ redux-middleware/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {createLogger} from 'redux-logger';
 import App from './App';
 import rootReducer from './modules';
 
-const store = createStore(rootReducer);
+const logger = createLogger()
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
