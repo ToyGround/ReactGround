01d527558135f5c806a3211fb3a1e21da3a472c2
baekCode <44539224+baekCode@users.noreply.github.com>
2021-03-24 22:50:49 +0900

    feat : provider

--- giphy-app/src/index.js
+++ giphy-app/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import App from './App';
+import rootReducer, {rootSaga} from './modules';
+
+
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 );
