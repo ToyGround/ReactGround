a61355f765c5caf3d8e860b7420e11192e4ff0e8
baekCode <baekcode@gmail.com>
2021-03-24 12:41:13 +0900

    feat : api lib

---
+++ giphy-app/src/lib/api.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import config from '../config';
+
+const {API, API_KEY} = config;
+
+/**
+ * @description createCallAPI 전달값에 따른 api 값 리턴 함수
+ * @param {string} kind [gifs, sticker]
+ * @param {string} type [search, random, trending]
+ * @returns {string}
+ * */
+const createCallAPI = ({kind, type}) => {
+  return `${API}/${kind}/${type}?api_key=${API_KEY}`;
+};
+
+/**
+ * @param {string} kind [gifs, sticker]
+ * @param {string} type [search, random, trending]
+ * @returns {object}
+ * */
+export const callAPI = async (kind, type) => {
+  const api = createCallAPI({kind, type});
+  const response = await axios(api);
+  return response.data;
+};
