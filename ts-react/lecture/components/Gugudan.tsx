74e49271270cb3c1b13df0ada5b1639795b65e1a
baekCode <baekcode@gmail.com>
2021-05-08 11:49:47 +0900

    feat : ts useRef

--- ts-react/lecture/components/Gugudan.tsx
+++ ts-react/lecture/components/Gugudan.tsx
@@ -1,20 +1,25 @@
 import * as React from 'react'
-import {useState} from 'react'
+import {useRef, useState} from 'react'
 
 export default function Gugudan() {
     const [first, setFirst] = useState(Math.ceil(Math.random() * 9))
     const [second, setSecond] = useState(Math.ceil(Math.random() * 9))
     const [value, setValue] = useState('')
     const [result, setResult] = useState('')
+    const inputEl = useRef<HTMLInputElement>(null)
 
     const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
+        const input = inputEl.current
         if (parseInt(value) === first * second) {
             setFirst(Math.ceil(Math.random() * 9))
             setSecond(Math.ceil(Math.random() * 9))
             setResult('정답입니다.')
+            if (input) input.focus()
+
         } else {
             setResult('땡!!')
+            if (input) input.focus()
         }
     }
 
@@ -26,7 +31,7 @@ export default function Gugudan() {
         <>
             <p>{first} 곱하기 {second}는?</p>
             <form onSubmit={onSubmit}>
-                <input type="number" onChange={onChange} value={value}/>
+                <input ref={inputEl} type="number" onChange={onChange} value={value}/>
             </form>
             <p>결과는? {result}</p>
         </>
