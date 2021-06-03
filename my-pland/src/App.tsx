2b8f12fe8e490db3722d0158deb4a81f0dc21a1b
baekCode <baekcode@gmail.com>
2021-06-03 23:37:19 +0900

    feat : Redirect & add HeaderPage

--- my-pland/src/App.tsx
+++ my-pland/src/App.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Redirect
 } from 'react-router-dom';
 import MainPage from './pages/MainPage';
+import HeaderPage from './pages/HeaderPage';
 
 function App() {
   return (
-    <>
+    <div className={'bg-emerald-100 h-screen'}>
       <Router>
         <Switch>
-          <Route path={'/'} component={MainPage}/>
+          <Route path={'/'} exact component={MainPage}/>
+          <Route path={'/components/header'} component={HeaderPage}/>
+          <Redirect from="*" to="/"/>
         </Switch>
       </Router>
-    </>
+    </div>
   );
 }
 
